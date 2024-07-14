3aa148f31ae9834fcd271352c5b5cc7857e61573
zYuna646 <anggarangga646@gmail.com>
2024-07-14 22:08:32 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use App\Livewire\RecapSurvei;
 
 Route::get('/login', Auth::class)->name('login');
 Route::get('/', Landing::class)->name('home');
-Route::get('/run_survei/{id}', RunSurvei::class)->name('run_survei');
+Route::get('/run_survei/{code}', RunSurvei::class)->name('run_survei');
 
 
 Route::middleware(['auth'])->group(function () {
